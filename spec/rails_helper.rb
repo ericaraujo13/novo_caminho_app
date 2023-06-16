780cfee31c50b775418951ca17422ba0f0200593
ericaraujo13 <ericpoloni@gmail.com>
2023-06-16 13:51:06 -0500

    fix

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,4 +17,5 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
