d06a4f6f348e53cd8c04fe5f3557c9aa808c2c06
ericaraujo13 <ericpoloni@gmail.com>
2023-06-20 10:39:50 -0500

    add filter by name in medicaments

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -3,4 +3,13 @@
 class Medicine < ApplicationRecord
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+
+  def self.ransackable_attributes(auth_object = nil)
+    ['name']
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["name"]
+  end
 end
